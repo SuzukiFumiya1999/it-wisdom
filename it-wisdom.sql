cba752fd3f941e90fe6ba311d0dd4b16e1d96f78
SuzukiFumiya <suzukipoke.makaron@gmail.com>
2022-04-20 16:12:49 +0900

    [Add] DB

---
+++ it-wisdom.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2022-04-20 08:15:01
+-- サーバのバージョン： 10.4.21-MariaDB
+-- PHP のバージョン: 7.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `itw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `name` varchar(255) DEFAULT NULL COMMENT 'ユーザー名',
+  `email` varchar(255) NOT NULL COMMENT 'メールアドレス',
+  `password` varchar(255) NOT NULL COMMENT 'パスワード'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`) VALUES
+(1, '管理者', 'admin@gmail.com', 'admin9999');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `goods`
+--
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `user_id` int(11) NOT NULL COMMENT 'ユーザーID',
+  `question_id` int(11) NOT NULL COMMENT '質問ID',
+  `created_at` datetime DEFAULT current_timestamp() COMMENT '登録日時'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `title` varchar(255) NOT NULL COMMENT '質問のタイトル',
+  `content` varchar(512) NOT NULL COMMENT '質問の内容',
+  `user_id` int(11) DEFAULT NULL COMMENT '登録者',
+  `image` varchar(512) DEFAULT NULL COMMENT '質問画像',
+  `del_flg` int(1) DEFAULT 0 COMMENT '削除フラグ',
+  `created_at` datetime DEFAULT current_timestamp() COMMENT '登録日時'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `replies`
+--
+
+CREATE TABLE `replies` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `content` varchar(512) NOT NULL COMMENT '回答内容',
+  `user_id` int(11) DEFAULT NULL COMMENT 'ユーザーID',
+  `question_id` int(11) DEFAULT NULL COMMENT '質問ID',
+  `image` varchar(255) DEFAULT NULL COMMENT '回答画像',
+  `del_flg` int(1) DEFAULT 0 COMMENT '削除フラグ',
+  `created_at` datetime DEFAULT current_timestamp() COMMENT '登録日時'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL COMMENT 'ID',
+  `name` varchar(255) DEFAULT NULL COMMENT 'ユーザー名',
+  `email` varchar(255) NOT NULL COMMENT 'メールアドレス',
+  `password` varchar(255) NOT NULL COMMENT 'パスワード',
+  `image` varchar(255) DEFAULT 'icon.png' COMMENT 'ユーザーイメージ'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=3;
+
+--
+-- テーブルの AUTO_INCREMENT `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=109;
+
+--
+-- テーブルの AUTO_INCREMENT `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=159;
+
+--
+-- テーブルの AUTO_INCREMENT `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=38;
+
+--
+-- テーブルの AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=48;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
