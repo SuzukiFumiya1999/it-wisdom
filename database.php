a0d3336c259d4c03c49f34720ed59e2b0610fd8d
SuzukiFumiya <suzukipoke.makaron@gmail.com>
2022-05-08 10:26:52 +0900

    [Add] database

--- database.php
+++ database.php
@@ -3,4 +3,3 @@ define('DB_HOST',       'localhost'); //データベースのホスト名又はI
 define('DB_USER',        'root'); //MySQLのユーザー名
 define('DB_PASSWD',   'makaron1009'); //MySQLのパスワード
 define('DB_NAME',       'itw'); //データベース名
-?>
